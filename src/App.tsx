61129ee1349c7bbf049ff29535edb85acb752953
Marc Betbeder <marc.betbeder@hotmail.com>
2024-09-22 21:28:27 +1000

    Dummy data displaying
    List of posts (imported manually from postman output, not through fetch yet) displaying on the home page. No links yet, but it displays all posts.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import posts from './data/posts.json';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const postsData = posts.data;
+	return (
+		<>
+			<header className="header">My Blog Posts!</header>
+			<main className="main">
+				<div className="container">
+					{postsData.map(post => (
+						<div className="post">
+							<h2 className="title">{post.title}</h2>
+						</div>
+					))}
+				</div>
+			</main>
+		</>
+	);
 }
 
 export default App;
